153e97dfe2460d85d9698fc044aea63d72466358
azvart <56933787+azvart@users.noreply.github.com>
2020-10-21 22:54:31 +0300

    ss

---
+++ precache-manifest.4c77592ac90bead31879da1c411c1925.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5612f4c5f41278af7fe93416d3b51ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6067e8adee9387ab8006",
+    "url": "/static/css/main.49ea9a90.chunk.css"
+  },
+  {
+    "revision": "c6a4f5fd375f45f8633f",
+    "url": "/static/js/2.11ade89c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.11ade89c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6067e8adee9387ab8006",
+    "url": "/static/js/main.9b5f2b03.chunk.js"
+  },
+  {
+    "revision": "75e0bf055d84e93f4503",
+    "url": "/static/js/runtime-main.19da2585.js"
+  }
+]);
